cbb568855bad6dc6d8d75a74a7b53e29af5a556c
Marcin Daczkowski <merdacz@gmail.com>
2016-06-17 11:02:18 +0200

    back to sproc

---
+++ OctopusPlayground.DB/Stored Procedures/dbo.TestFreeText.sql
@@ -0,0 +1,16 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+
+CREATE PROCEDURE [dbo].[TestFreeText]
+	@term nvarchar (200)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT * FROM [Events] WHERE CONTAINS([Description], @term)
+END
+
+GO
