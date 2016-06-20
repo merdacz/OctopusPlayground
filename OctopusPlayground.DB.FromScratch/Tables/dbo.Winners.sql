980e8eb5779455cf6ad7e4ffcd4f5acf4a2bf039
Marcin Daczkowski <merdacz@gmail.com>
2016-06-20 20:23:48 +0200

    some migration, will this affect order?

--- OctopusPlayground.DB.FromScratch/Tables/dbo.Winners.sql
+++ OctopusPlayground.DB.FromScratch/Tables/dbo.Winners.sql
@@ -2,8 +2,9 @@ CREATE TABLE [dbo].[Winners]
 (
 [Id] [int] NOT NULL,
 [ParticipantId] [int] NOT NULL,
-[Prize] [nvarchar] (50) COLLATE Polish_CI_AS NULL
-) ON [PRIMARY]
+[Prize] [nvarchar] (50) COLLATE Polish_CI_AS NULL,
+[Comment] [nvarchar] (max) COLLATE Polish_CI_AS NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Winners] ADD CONSTRAINT [PK_Winners] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
 GO
