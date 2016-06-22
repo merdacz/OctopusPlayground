087211957289ced5c207b1a42cbf28fa15506050
Marcin Daczkowski <merdacz@gmail.com>
2016-06-22 10:57:50 +0200

    new test column

--- OctopusPlayground.DB.FromScratch/Tables/dbo.Winners.sql
+++ OctopusPlayground.DB.FromScratch/Tables/dbo.Winners.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[Winners]
 [ParticipantId] [int] NOT NULL,
 [Prize] [nvarchar] (50) COLLATE Polish_CI_AS NULL,
 [Test] [bit] NULL,
-[Comments] [nvarchar] (100) COLLATE Polish_CI_AS NOT NULL
+[Comments] [nvarchar] (100) COLLATE Polish_CI_AS NOT NULL,
+[Test2] [bit] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Winners] ADD CONSTRAINT [PK_Winners] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
