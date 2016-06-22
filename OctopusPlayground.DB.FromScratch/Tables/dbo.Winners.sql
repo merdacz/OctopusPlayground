5212b998d2183ca5c6de3623759d9f7c4ea62c81
Marcin Daczkowski <merdacz@gmail.com>
2016-06-22 10:43:02 +0200

    minor change

--- OctopusPlayground.DB.FromScratch/Tables/dbo.Winners.sql
+++ OctopusPlayground.DB.FromScratch/Tables/dbo.Winners.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[Winners]
 (
 [Id] [int] NOT NULL,
 [ParticipantId] [int] NOT NULL,
-[Prize] [nvarchar] (50) COLLATE Polish_CI_AS NULL
+[Prize] [nvarchar] (50) COLLATE Polish_CI_AS NULL,
+[Test] [bit] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Winners] ADD CONSTRAINT [PK_Winners] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
