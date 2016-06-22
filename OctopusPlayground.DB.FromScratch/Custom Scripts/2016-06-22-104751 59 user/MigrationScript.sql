c80483014c3d7873092521a51de0f5468bb6e706
Marcin Daczkowski <merdacz@gmail.com>
2016-06-22 10:47:51 +0200

    some migration

---
+++ OctopusPlayground.DB.FromScratch/Custom Scripts/2016-06-22-104751 59 user/MigrationScript.sql
@@ -0,0 +1,40 @@
+/*
+This migration script replaces uncommitted changes made to these objects:
+Winners
+
+Use this script to make necessary schema and data changes for these objects only. Schema changes to any other objects won't be deployed.
+
+Schema changes and migration scripts are deployed in the order they're committed.
+*/
+
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Dropping constraints from [dbo].[Winners]'
+GO
+ALTER TABLE [dbo].[Winners] DROP CONSTRAINT [PK_Winners]
+GO
+PRINT N'Rebuilding [dbo].[Winners]'
+GO
+CREATE TABLE [dbo].[RG_Recovery_1_Winners]
+(
+[Id] [int] NOT NULL,
+[ParticipantId] [int] NOT NULL,
+[Prize] [nvarchar] (50) COLLATE Polish_CI_AS NULL,
+[Test] [bit] NULL,
+[Comments] [nvarchar] (100) COLLATE Polish_CI_AS NOT NULL
+) ON [PRIMARY]
+GO
+INSERT INTO [dbo].[RG_Recovery_1_Winners]([Id], [ParticipantId], [Prize], [Test], [Comments])
+SELECT [Id], [ParticipantId], [Prize], [Test], 'some new test comment' FROM [dbo].[Winners]
+GO
+DROP TABLE [dbo].[Winners]
+GO
+EXEC sp_rename N'[dbo].[RG_Recovery_1_Winners]', N'Winners', N'OBJECT'
+GO
+PRINT N'Creating primary key [PK_Winners] on [dbo].[Winners]'
+GO
+ALTER TABLE [dbo].[Winners] ADD CONSTRAINT [PK_Winners] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
+GO
+
--- OctopusPlayground.DB.FromScratch/Tables/dbo.Winners.sql
+++ OctopusPlayground.DB.FromScratch/Tables/dbo.Winners.sql
@@ -3,7 +3,8 @@ CREATE TABLE [dbo].[Winners]
 [Id] [int] NOT NULL,
 [ParticipantId] [int] NOT NULL,
 [Prize] [nvarchar] (50) COLLATE Polish_CI_AS NULL,
-[Test] [bit] NULL
+[Test] [bit] NULL,
+[Comments] [nvarchar] (100) COLLATE Polish_CI_AS NOT NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Winners] ADD CONSTRAINT [PK_Winners] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
